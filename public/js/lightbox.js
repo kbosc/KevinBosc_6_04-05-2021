c2f56ec258f4c4eab6f299fe1710a1c925a7fa32
Kevin Bosc <kevin_bosc@hotmail.fr>
2021-08-03 20:18:40 +0200

    mise en place de la lightbox

---
+++ public/js/lightbox.js
@@ -0,0 +1,46 @@
+class Lightbox {
+  static init() {
+    const links = document
+      //   .querySelectorAll('a[href$=".jpg"], a[href$=".mp4"]')
+      .querySelectorAll('portfolio-card-img[src$=".jpg"]')
+      .forEach((link) =>
+        link.addEventListener("click", (e) => {
+          e.preventDefault();
+          //   new Lightbox(e.currentTarget.getAttribute("href"));
+          new Lightbox(e.currentTarget.getAttribute("src"));
+        })
+      );
+  }
+
+  // @param {string} url Url de l'image
+  constructor(url) {
+    const element = this.buildDom(url);
+    document.body.appendChild(element);
+  }
+  // @param {string} url Url de l'image
+  // @return {HTMLElement}
+  buildDom(url) {
+    const dom = document.createElement("div");
+    dom.classList.add("lightbox");
+    dom.innerHTML = `<button class="lightbox__close">Fermer</button>
+    <button class="lightbox__next">Suivant</button>
+    <button class="lightbox__prev">Précédent</button>
+    <div class="lightbox__container">
+      <img src="https://picsum.photos/900/1800" alt="" />
+    </div>`;
+    return dom;
+  }
+}
+
+/* 
+    <div class="lightbox">
+      <button class="lightbox__close">Fermer</button>
+      <button class="lightbox__next">Suivant</button>
+      <button class="lightbox__prev">Précédent</button>
+      <div class="lightbox__container">
+        <img src="https://picsum.photos/900/1800" alt="" />
+      </div>
+    </div> 
+*/
+
+Lightbox.init();
--- public/js/photographer-page.js
+++ public/js/photographer-page.js
@@ -7,6 +7,8 @@ console.log(photographerId);
 if (!photographerId) {
   window.location = "/public";
 }
+let mediaPhotographer = [];
+let currentPhotographer;
 // -----------------Test---------------------
 (async () => {
   const res = await fetch("./json/data.json");
@@ -37,6 +39,21 @@ if (!photographerId) {
 
   function openModalImg() {
     modalImg.style.display = "block";
+    console.log(this);
+    console.log(mediaPhotographer);
+    let imgId = this.getAttribute("data-img-id");
+    console.log(mediaPhotographer[imgId]);
+    if (mediaPhotographer[imgId] != undefined) {
+      this.classList.add("active");
+      document
+        .getElementById("modal-img")
+        .setAttribute(
+          "src",
+          `img/photos/${currentPhotographer.name}/${mediaPhotographer[imgId].image}`
+        );
+      document.getElementById("modal-title").innerHTML =
+        mediaPhotographer[imgId].alt;
+    }
   }
   document.querySelectorAll(".portfolio__card__img").forEach((element) => {
     element.addEventListener("click", openModalImg);
@@ -63,6 +80,7 @@ function addPhotographerResume(photographers, media) {
     window.open("index.html", "_self");
   }
   console.log(found);
+  currentPhotographer = found;
   const photographerWrapper = document.getElementById("photographer-wrapper");
   const arrayTags = found.tags;
   photographerWrapper.innerHTML = `
@@ -95,42 +113,79 @@ function addPhotographerResume(photographers, media) {
 
   const portfolioWrapper = document.getElementById("portfolio-wrapper");
   let cards = "";
-  console.log(cards);
   media.forEach((media) => {
     const photographerIdMedia = media.photographerId;
     const parsedMedia = parseInt(photographerIdMedia, 10);
     if (parsed == parsedMedia) {
-      cards += `<div class="portfolio__card">
-      <a href="#" class="portfolio__card__img">
-        <img
-          src="img/photos/${found.name}/${media.image}"
-          alt=""
-          class="portfolio-card-img"
-        />
-      </a>
-      <div class="portfolio__card__about">
-        <p>${media.title}</p>
-        <div class="count-like">
-          <span>${media.likes}</span>
-          <!-- <p>12</p> -->
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-icon="heart"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            class="like"
-            aria-label="like"
-          >
-            <path
-              fill="currentColor"
-              d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"
-            ></path>
-          </svg>
+      mediaPhotographer[media.id] = media;
+      if (media.video) {
+        cards += `<div class="portfolio__card">
+        <a href="#" class="portfolio__card__img" data-img-id="${media.id}">
+          <video
+          controls
+            src="img/photos/${found.name}/${media.video}"
+            alt=""
+            class="portfolio-card-img"
+          /video>
+        </a>
+        <div class="portfolio__card__about">
+          <p>${media.title}</p>
+          <div class="count-like">
+            <span>${media.likes}</span>
+            <!-- <p>12</p> -->
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-icon="heart"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              class="like"
+              aria-label="like"
+            >
+              <path
+                fill="currentColor"
+                d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"
+              ></path>
+            </svg>
+          </div>
         </div>
-      </div>
-    </div>`;
+      </div>`;
+      }
+      if (media.image) {
+        cards += `<div class="portfolio__card">
+        <a href="#" class="portfolio__card__img" data-img-id="${media.id}">
+          <img
+            src="img/photos/${found.name}/${media.image}"
+            alt=""
+            class="portfolio-card-img"
+          />
+        </a>
+        <div class="portfolio__card__about">
+          <p>${media.title}</p>
+          <div class="count-like">
+            <span>${media.likes}</span>
+            <!-- <p>12</p> -->
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-icon="heart"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              class="like"
+              aria-label="like"
+            >
+              <path
+                fill="currentColor"
+                d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>`;
+      }
+      // test-------------
     }
   });
   portfolioWrapper.innerHTML = cards;
